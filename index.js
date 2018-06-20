4c2aad6dddb7f14d0699f6c0844202969a696cfe
Sriya Aluru <sriyaaluru@gmail.com>
2018-06-20 18:05:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Write your code in this file!
-
+var currentUser = 'Sriya';
+var welcomeMessage = "Welcome to Klossybook, " + currentUser + "!";
+var excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, "+ currentUser.toUpperCase() +"!";
+var shortGreeting = "Welcome, " + currentUser.charAt(0) +"!"; 
