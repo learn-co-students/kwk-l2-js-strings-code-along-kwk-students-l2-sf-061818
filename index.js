d63843236e069adfbd43860685dec84e0412b0ea
ashchu <ashchu890@gmail.com>
2018-08-08 14:24:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // Write your code in this file!
-
+currentUser = "Ashley";
+welcomeMessage = "Welcome to Klossybook, ";
+welcomeMessage = welcomeMessage + currentUser + "!";
+excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, " + currentUser.toUpperCase() + "!";
+shortGreeting = "Welcome, "+ currentUser.substring(0,1) + "!";
